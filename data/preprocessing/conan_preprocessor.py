d88a831e05856e59672f3a0480f4bea16ae79945
Shane Weisz <shaneweisz@gmail.com>
2022-05-23 12:17:14 +0100

    Extract only English examples from CONAN.
    Also updates summary statistics accordingly.

--- data/preprocessing/conan_preprocessor.py
+++ data/preprocessing/conan_preprocessor.py
@@ -9,10 +9,23 @@ class ConanPreprocessor(Preprocessor):
         df = pd.read_csv(file_path)
         hs_cs_pairs = []
         for _, row in df.iterrows():
-            hs = row["hateSpeech"]
-            cs = row["counterSpeech"]
-            hs_cs_pairs.append((hs, cs))
+            cn_id = row["cn_id"]
+            if self._is_english(cn_id):
+                hs = row["hateSpeech"]
+                cs = row["counterSpeech"]
+                hs_cs_pairs.append((hs, cs))
         return hs_cs_pairs
+
+    @staticmethod
+    def _is_english(cn_id):
+        """
+        cn_id: counter narrative id
+            Example: "ENT1ST0001HS0033CN000021" (originally english)
+                  or "ITT1ST0023HS0055CN001068T1" (translated to english from italian)
+        """
+        originally_english = cn_id[:2] == "EN"
+        translated_to_english = cn_id[-2:] == "T1"
+        return originally_english or translated_to_english
 
 
 class MultiTargetConanPreprocessor(Preprocessor):
