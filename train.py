8d7af4ddcd6a5531c75c79c8b40b2d0fefbe5c75
Shane Weisz <shaneweisz@gmail.com>
2022-06-01 12:08:53 +0100

    Set batch sizes to make better use of GPU

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 from datasets import DatasetDict, load_dataset, Dataset
 from transformers import (
     AutoModelForCausalLM,
@@ -16,14 +16,14 @@ wandb.init(project="AutoCounterspeech")
 
 
 def main():
-    print(f"Loading model from {args.model_name}")
-    model = load_model(args.model_name)
-    print(f"Loading tokenizer from {args.model_name}")
-    tokenizer = load_tokenizer(args.model_name)
+    print(f"Loading model from {args.pretrained_model_name_or_path}")
+    model = load_model(args.pretrained_model_name_or_path)
+    print(f"Loading tokenizer from {args.pretrained_model_name_or_path}")
+    tokenizer = load_tokenizer(args.pretrained_model_name_or_path)
 
     training_args = training_args_from_config(args.training_config_path)
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-    print("Preparing and tokenizing train and val datasets")
+    print("Loading and tokenizing train and val datasets")
     train_dataset, val_dataset = prepare_data_for_training(args.data_dir, tokenizer)
 
     trainer = Trainer(
@@ -38,14 +38,20 @@ def main():
     trainer.train()
 
 
+def load_model(pretrained_model_name_or_path: str) -> AutoModelForCausalLM:
+    return AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path)
+
+
 def load_tokenizer(pretrained_model_name_or_path: str) -> AutoTokenizer:
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
     tokenizer.pad_token = tokenizer.eos_token
     return tokenizer
 
 
-def load_model(pretrained_model_name_or_path: str) -> AutoModelForCausalLM:
-    return AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path)
+def training_args_from_config(config_path: Path) -> TrainingArguments:
+    training_params = json.load(open(config_path))
+    training_args = TrainingArguments(**training_params)
+    return training_args
 
 
 def prepare_data_for_training(
@@ -57,8 +63,9 @@ def prepare_data_for_training(
     return train_dataset, val_dataset
 
 
-def load_raw_datasets(data_dir: Path) -> DatasetDict:
-    splits = ["train", "val"]
+def load_raw_datasets(
+    data_dir: Path, splits: List[str] = ["train", "val"]
+) -> DatasetDict:
     data_files = {split: str(data_dir / f"{split}.csv") for split in splits}
     raw_datasets = load_dataset("csv", data_files=data_files)
     return raw_datasets
@@ -66,28 +73,31 @@ def load_raw_datasets(data_dir: Path) -> DatasetDict:
 
 def tokenize(raw_datasets: DatasetDict, tokenizer: AutoTokenizer) -> DatasetDict:
     def tokenize_row(row):
-        text = row["hate_speech"]
-        text += tokenizer.eos_token
-        text += row["counter_speech"]
-        return tokenizer(text)
+        hs = row["hate_speech"]
+        cs = row["counter_speech"]
+        EOS = tokenizer.eos_token
+
+        text = f"{hs}{EOS}{cs}{EOS}"
+        tokenized_text = tokenizer(text)
+
+        return tokenized_text
 
     return raw_datasets.map(
         tokenize_row, remove_columns=raw_datasets["train"].column_names
     )
 
 
-def training_args_from_config(config_path: Path) -> TrainingArguments:
-    training_params = json.load(open(config_path))
-    training_args = TrainingArguments(**training_params)
-    return training_args
-
+DIALO_GPT_MEDIUM = "microsoft/DialoGPT-medium"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model", dest="model_name", type=str, default="microsoft/DialoGPT-medium"
+        "-m",
+        "--pretrained_model_name_or_path",
+        type=str,
+        default=DIALO_GPT_MEDIUM,
     )
-    parser.add_argument("--config", dest="training_config_path", type=Path)
+    parser.add_argument("-c", "--config", dest="training_config_path", type=Path)
     parser.add_argument(
         "--data_dir", type=Path, default="data/splits/multitarget-conan"
     )
