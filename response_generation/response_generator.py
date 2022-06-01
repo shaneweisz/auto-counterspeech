ffc69e3ea95582b571712267b04ad52cbe67cde3
Shane Weisz <shaneweisz@gmail.com>
2022-06-01 17:08:00 +0100

    Add batch generation

--- response_generation/response_generator.py
+++ response_generation/response_generator.py
@@ -16,9 +16,39 @@ class ResponseGenerator:
         self.model = self.model.to(device)
         self.decoding_config = decoding_config
 
-    def generate_responses(self, inputs: List[str]) -> List[str]:
-        responses = [self.generate_response(hs_input) for hs_input in tqdm(inputs)]
+    def generate_responses(self, inputs: List[str], batch_size=1) -> List[str]:
+        responses = []
+        for i in tqdm(range(0, len(inputs), batch_size)):
+            batch_inputs = inputs[i : i + batch_size]
+            batch_responses = self.generate_responses_for_batch(batch_inputs)
+            responses.extend(batch_responses)
         return responses
+
+    def generate_responses_for_batch(self, batch_inputs: List[str]) -> str:
+        # This is based on:
+        # https://discuss.huggingface.co/t/batch-generation-with-gpt2/1517/3
+
+        self.tokenizer.padding_side = "left"
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        batch_inputs = [item + self.tokenizer.eos_token for item in batch_inputs]
+        tokenized_batch_inputs = self.tokenizer(
+            batch_inputs, return_tensors="pt", padding=True
+        ).to(device)
+
+        generated_ids = self.model.generate(
+            **tokenized_batch_inputs,
+            pad_token_id=self.tokenizer.eos_token_id,
+            **self.decoding_config,
+        )
+
+        max_input_length = tokenized_batch_inputs["input_ids"].shape[-1]
+        generated_ids = generated_ids[:, max_input_length:]
+        batch_responses = self.tokenizer.batch_decode(
+            generated_ids, skip_special_tokens=True
+        )
+
+        return batch_responses
 
     def generate_response(self, hs_input: str) -> str:
         input_with_eos_token = hs_input + self.tokenizer.eos_token
