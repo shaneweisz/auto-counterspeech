ecdc253b5a36aac51899692482351d2cada8c8f8
Shane Weisz <shaneweisz@gmail.com>
2022-06-07 13:49:50 +0100

    Add toxicity scores to all experiments

---
+++ experiments/aggregate-results.sh
@@ -0,0 +1,25 @@
+# NB: Run from base directory
+
+RESULTS_CSV=experiments/results.csv
+
+# extract metrics from all words left of ":" using grep
+EVAL_FILES=`find experiments -name *predictions.eval.txt -not -path *archive*`
+for f in $EVAL_FILES; do
+    metric_names=$(cat $f | sed 's/:.*//g')
+done
+
+# print metric names separated by commas
+echo -n "Model," > $RESULTS_CSV # since first column will be the model name
+echo $metric_names | sed 's/ /,/g' >> $RESULTS_CSV
+
+# extract scores from each file and print them separated by commas
+for f in $EVAL_FILES; do
+    model_name=$(basename $(dirname $f))
+    echo -n $model_name >> $RESULTS_CSV
+    echo -n "," >> $RESULTS_CSV
+
+    scores=$(grep -oE '[0-9]+\.[0-9]+' $f)
+    echo $scores | sed 's/ /,/g' >> $RESULTS_CSV
+done
+
+python experiments/format-results.py
--- experiments/format-results.py
+++ experiments/format-results.py
@@ -4,8 +4,11 @@ results_csv = "/home/sw984/rds/hpc-work/mphil-project/auto-counterspeech/experim
 
 df = pd.read_csv(results_csv)
 
+# Reorder columns
+df = df[['Model', 'GRUEN', 'BLEU-2', 'BERTScore', 'Toxicity', 'Dist-1', 'Dist-2', 'Ent-4', 'AvgLen']]
+
 # Format columns as percentages
-COLS = ["GRUEN", "BERTScore", "BLEU-2", "Dist-1", "Dist-2"]
+COLS = ["GRUEN", "BERTScore", "Toxicity", "BLEU-2", "Dist-1", "Dist-2"]
 for col in COLS:
     df[col] = df[col] * 100
     df.rename(columns={col: col + "(%)"}, inplace=True)
@@ -13,5 +16,18 @@ for col in COLS:
 non_modelname_cols = [col for col in df.columns if col != "Model"]
 for col in non_modelname_cols:
     df[col] = round(df[col], 2)
+
+# replace row with model name == "oracle" with N/A for BERTScore and BLEU-2
+df.loc[df["Model"] == "oracle", "BERTScore(%)"] = "N/A"
+df.loc[df["Model"] == "oracle", "BLEU-2(%)"] = "N/A"
+
+# change model names to be more readable
+df.loc[df["Model"] == "oracle", "Model"] = "Oracle"
+df.loc[df["Model"] == "dialoGPT-mtconan-beam10-lengthpen15_0.9", "Model"] = "DialoGPT-finetuned"
+df.loc[df["Model"] == "dialoGPT-outofthebox-beam10", "Model"] = "DialoGPT-outofthebox"
+df.loc[df["Model"] == "GPS-3000epochs", "Model"] = "GPS"
+
+df["Model"] = pd.Categorical(df["Model"], ["DialoGPT-outofthebox", "GPS", "DialoGPT-finetuned", "Oracle"])
+df = df.sort_values(by=["Model"])
 
 df.to_csv(results_csv, index=False)
