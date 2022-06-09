c92bda136b686c96f2f82f9a8dae5c0c5025aaa7
Shane Weisz <shaneweisz@gmail.com>
2022-06-09 17:54:35 +0100

    Remove unnecessary functions from training script

--- train.py
+++ train.py
@@ -16,16 +16,22 @@ wandb.init(project="AutoCounterspeech")
 
 
 def main():
-    print(f"Loading model from {args.pretrained_model_name_or_path}")
-    model = load_model(args.pretrained_model_name_or_path)
-    print(f"Loading tokenizer from {args.pretrained_model_name_or_path}")
-    tokenizer = load_tokenizer(args.pretrained_model_name_or_path)
+    print(f"Loading model: {args.model_name_or_path}")
+    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
 
-    training_args = training_args_from_config(args.training_config_path)
+    print(f"Loading tokenizer: {args.model_name_or_path}")
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    print(f"Setting up training params using: {args.data_dir}")
+    training_params = json.load(open((args.config_path)))
+    training_args = TrainingArguments(**training_params)
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-    print("Loading and tokenizing train and val datasets")
+
+    print(f"Loading and tokenizing train and val datasets stored in: {args.data_dir}")
     train_dataset, val_dataset = prepare_data_for_training(args.data_dir, tokenizer)
 
+    print("Setup Trainer")
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -34,40 +40,22 @@ def main():
         eval_dataset=val_dataset,
         tokenizer=tokenizer,
     )
+
     print("Start training")
     trainer.train()
+
     print("Training finished. Saving best model.")
     trainer.save_model()
 
 
-def load_model(pretrained_model_name_or_path: str) -> AutoModelForCausalLM:
-    return AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path)
-
-
-def load_tokenizer(pretrained_model_name_or_path: str) -> AutoTokenizer:
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
-    tokenizer.pad_token = tokenizer.eos_token
-    return tokenizer
-
-
-def training_args_from_config(config_path: Path) -> TrainingArguments:
-    training_params = json.load(open(config_path))
-    training_args = TrainingArguments(**training_params)
-    return training_args
-
-
-def prepare_data_for_training(
-    data_dir: Path, tokenizer: AutoTokenizer
-) -> Tuple[Dataset, Dataset]:
+def prepare_data_for_training(data_dir: Path, tokenizer: AutoTokenizer) -> Tuple[Dataset, Dataset]:
     raw_datasets = load_raw_datasets(data_dir)
     tokenized_datasets = tokenize(raw_datasets, tokenizer)
     train_dataset, val_dataset = tokenized_datasets["train"], tokenized_datasets["val"]
     return train_dataset, val_dataset
 
 
-def load_raw_datasets(
-    data_dir: Path, splits: List[str] = ["train", "val"]
-) -> DatasetDict:
+def load_raw_datasets(data_dir: Path, splits: List[str] = ["train", "val"]) -> DatasetDict:
     data_files = {split: str(data_dir / f"{split}.csv") for split in splits}
     raw_datasets = load_dataset("csv", data_files=data_files)
     return raw_datasets
@@ -84,24 +72,23 @@ def tokenize(raw_datasets: DatasetDict, tokenizer: AutoTokenizer) -> DatasetDict
 
         return tokenized_text
 
-    return raw_datasets.map(
-        tokenize_row, remove_columns=raw_datasets["train"].column_names
-    )
+    return raw_datasets.map(tokenize_row, remove_columns=raw_datasets["train"].column_names)
 
-
-DIALO_GPT_MEDIUM = "microsoft/DialoGPT-medium"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    DIALO_GPT_MEDIUM = "microsoft/DialoGPT-medium"
     parser.add_argument(
         "-m",
-        "--pretrained_model_name_or_path",
+        "--model_name_or_path",
         type=str,
         default=DIALO_GPT_MEDIUM,
     )
-    parser.add_argument("-c", "--config", dest="training_config_path", type=Path)
-    parser.add_argument(
-        "--data_dir", type=Path, default="data/splits/multitarget-conan"
-    )
+
+    parser.add_argument("-c", "--config", dest="config_path", default="config/train.config.json", type=Path)
+    parser.add_argument("-d", "--data_dir", type=Path, default="data/splits/multitarget-conan")
+
     args = parser.parse_args()
+
     main()
