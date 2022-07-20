1d8c717c0a74771aefb726658791dad113d366f7
Shane Weisz <shaneweisz@gmail.com>
2022-07-20 16:36:57 +0100

    Fix bug in interrater reliability

--- human_evaluation/compute_inter_rater_reliability.py
+++ human_evaluation/compute_inter_rater_reliability.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from krippendorff import alpha
 
-results_file = "human_eval/results.tsv"
+results_file = "human_evaluation/results.tsv"
 reliability_data = pd.read_csv(results_file, sep="\t", header=None).to_numpy()
-score = alpha(reliability_data=reliability_data, level_of_measurement="ordinal")
+score = alpha(reliability_data=reliability_data, level_of_measurement="ordinal", value_domain=[1, 2, 3, 4, 5])
 print(f"Krippendorff's alpha: {score:.2f}")
